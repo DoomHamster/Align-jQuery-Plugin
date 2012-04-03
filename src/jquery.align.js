fec161a2a36d6f1c81c93729521419dc2234aaf1
Bradley Mountford <doomhamster@gmail.com>
2012-04-03 16:07:58 -0600

    Actually committing the working version this time...

--- src/jquery.align.js
+++ src/jquery.align.js
@@ -11,48 +11,58 @@
 
 (function($) {
 
-	$.fn.align = function(options) {
-		var container = window;
-		var y = true;
-		var x = true;
-		var speed = 0;
+    $.fn.align = function(options) {
+        var alignY = true;
+        var alignX = true;
+        var animateOptions = {};
+        
+        if (options) {
+            if (typeof(options.alignY) == "boolean") alignY = options.alignY;
+            if (typeof(options.alignX) == "boolean") alignX = options.alignX;
+            if (options.duration || options.duration === 0) animateOptions.duration = options.duration;
+            if (options.easing) animateOptions.easing = options.easing;
+            if (options.complete) animateOptions.complete = options.complete;
+            if (options.step) animateOptions.step = options.step;
+            if (typeof(options.queue) == "boolean") animateOptions.queue = options.queue;
+            if (options.specialEasing) animateOptions.specialEasing = options.specialEasing;
+        }
 
-		if (options) {
-			if (options.container) container = $(options.container);
-			if (options.y != undefined && options.y != null && typeof (options.y) == "boolean") y = options.y;
-			if (options.x != undefined && options.x != null && typeof (options.x) == "boolean") horizontal = options.x;
-			if (options.speed && !isNaN(options.speed)) speed = options.speed;
-		}
-
-		return this.each(function() {
-			var h = $(this).outerHeight();
-			var w = $(this).outerWidth();
-			if (h < 1 || w < 1) return false; //exit if height or width is 0
-			var ch; //container height
-			var cw; //container width
-			var top;
-			var left;
-			if (container == window || container == document) {
-				ch = $(container).height();
-				cw = $(container).width();
-				top = (ch - h) / 2;
-				left = (cw - w) / 2;
-			}
-			else {
-				var offset = $(container).offset();
-				ch = $(container).innerHeight();
-				cw = $(container).innerWidth();
-				top = ((ch - h) / 2) + offset.top;
-				left = ((cw - w) / 2) + offset.left;
-			}
-			var axes = {};
-
-			if (y) axes.top = top;
-			if (x) axes.left = left;
-
-			$(this).css("position", "absolute");
-			$(this).animate(axes, speed);
-		});
-	};
+        return this.each(function() {
+            var offsetParent = $(this).offsetParent();
+            var container;
+            if (options.container) container = $(options.container);
+            else container = $(this).parent();
+            
+            var h = $(this).outerHeight();
+            var w = $(this).outerWidth();
+            if (h < 1 || w < 1) return false; //exit if height or width is 0
+            
+            var ch; //container height
+            var cw; //container width
+            var top;
+            var left;
+            
+            if (container[0] == window || container[0] == document || offsetParent[0] == container[0]) {
+                ch = $(container).height();
+                cw = $(container).width();
+                top = (ch - h) / 2;
+                left = (cw - w) / 2;
+            }
+            else {
+                var offset = $(container).offset();
+                ch = $(container).innerHeight();
+                cw = $(container).innerWidth();
+                top = ((ch - h) / 2) + offset.top;
+                left = ((cw - w) / 2) + offset.left;
+            }
+            
+            var properties = {};
+            if (alignY) properties.top = top;
+            if (alignX) properties.left = left;
+            
+            $(this).css("position", "absolute");
+            $(this).animate(properties, animateOptions);
+        });
+    };
 
 })(jQuery);
